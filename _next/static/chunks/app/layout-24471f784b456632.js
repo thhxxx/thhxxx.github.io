5eb8f396daad098f025318e4134e86831c11b787
thhxxx <64131914+thhxxx@users.noreply.github.com>
2025-09-22 11:11:17 +0700

    Add files via upload

---
+++ _next/static/chunks/app/layout-24471f784b456632.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3673:()=>{},5181:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,3673,23))}},e=>{e.O(0,[978,619,441,255,358],()=>e(e.s=5181)),_N_E=e.O()}]);
